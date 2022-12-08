442a04af95a65f952e502eea42a0594cdaa0c682
gabrielm <gabrielmj2312@gmail.com>
2022-12-08 11:25:39 -0400

    Correccion de prototipos

--- xor_list.h
+++ xor_list.h
@@ -1,38 +1,44 @@
 /*
- * Tipos de datos:
+ * Tipos de datos
  */
 typedef struct node {
-	double valor;
+	int valor;
 	struct node *seq_ptr;
 }
-Node;
+node_t;
 
-typedef struct lista {
-	Node *head, *last;	
+typedef struct {
+	Node *ini, *fin;	
 }
-Lista;
+lista_t;
 
 /*
  * Macros para navegar la lista
  */
-#define NEXT(cur,prev) cur->seq_ptr ^ prev->seq_ptr
-#define PREV(cur,next) cur->seq_ptr ^ next->seq_ptr
+#define NEXT(cur,prev) (cur->seq_ptr ^ prev)
+#define PREV(cur,next) (cur->seq_ptr ^ next)
 
 /*
  * Funciones de utilidad
  */
-Node *nuevo_nodo(double elem);
-Lista *nueva_lista();
+node_t *nuevo_nodo(double elem);
+lista_t *nueva_lista(void);
+void elimina_lista(lista_t *listp);
 
 /*
  * Funciones del TAD
  */
-void insertar_inicio(Lista *listp, double elem);
-void insertar_final(Lista *listp, double elem);
-void insertar_ordenado(Lista *listp, double elem);
-void concatenar(Lista *l1, Lista *l2, Lista *l3);
-void invertir(Lista *listp, Lista *revp);
-int vacia(Lista *listp);
-void borrar_primero(Lista *listp);
-void borrar_ultimo(Lista *listp);
-void borrar_primera_ocurrencia(Lista *listp, double elem);
+// Insertar al inicio, final y ordenado
+void inserta_ilista(lista_t *listp, int elem);
+void inserta_flista(lista_t *listp, int elem);
+void inserta_lista(lista_t *listp, int elem);
+// Sacar el primero, ultimo y primera ocurrencia
+void sacarp_lista(lista_t *listp, int *elem);
+void sacarf_lista(lista_t *listp, int *elem);
+int sacar_lista(lista_t *listp, int elem);
+// Funciones variadas
+int buscar_lista(lista_t *listp, int elem);
+void imprime_lista(lista_t *listp);
+void concatenar(lista_t *l1, lista_t *l2, lista_t *l3);
+void invertir(lista_t *listp, lista_t *revp);
+int lista_vacia(lista_t *listp);
